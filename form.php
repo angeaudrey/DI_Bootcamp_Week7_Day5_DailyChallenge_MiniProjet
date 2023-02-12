5db81c7f682fa2b6bfa9220eb4016cfcaba7b4e6
raissa <kouabenanraissa@yahoo.fr>
2023-02-12 19:34:36 +0000

    first commit

---
+++ form.php
@@ -0,0 +1,7 @@
+<form action="sendmail.php" method="post">
+    <input type="email" name="to" placeholder="to"><br><br>
+    <input type="email"  name="from" placeholder="from"><br><br>
+    <input type="text"  name="subject" placeholder="Subject"><br><br>
+    <textarea name="message" placeholder="message"></textarea><br><br>
+    <input type="submit" value="Envoyer">
+</form>
